5e52181073c08977f721daaac492e8a7003eb7f6
atis2-afk <konstantinoskatsandredakis@gmail.com>
2026-02-22 11:39:43 +0200

    Add files via upload

---
+++ Guessinggame.py
@@ -0,0 +1,11 @@
+import random
+from random import randint
+
+x = randint(a=1, b=10)
+print(x)
+y = int(input("guess a number"))
+while y != x:
+    print("try again!")
+    x = int(input("next guess?"))
+if y == x:
+    print("yay")
